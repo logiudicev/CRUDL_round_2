8c27d2714e9bbe7977fcf8295c9ec19920cd86d3
Vincent R. LoGiudice <vincent.r.logiudice@gmail.com>
2021-03-12 13:46:02 -0600

    new stuff new day

---
+++ src/main/java/Mil/Software/CRUDLround2/CrudLround2Application.java
@@ -0,0 +1,13 @@
+package Mil.Software.CRUDLround2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudLround2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudLround2Application.class, args);
+	}
+
+}
---
+++ src/test/java/Mil/Software/CRUDLround2/CrudLround2ApplicationTests.java
@@ -0,0 +1,13 @@
+package Mil.Software.CRUDLround2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudLround2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
